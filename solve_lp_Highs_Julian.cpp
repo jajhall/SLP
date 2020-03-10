631ac3cd3abfd86684276a8fb0ebc62cd6b02d4f
Julian Hall <jajhall@ed.ac.uk>
2020-03-10 17:12:51 +0000

    About to debug new cases from AGR

--- solve_lp_Highs_Julian.cpp
+++ solve_lp_Highs_Julian.cpp
@@ -8,6 +8,7 @@
 #include "HConst.h"
 #include "Highs.h"
 #include "HighsStatus.h"
+#include "HighsInfo.h"
 #include "HighsLp.h"
 
 // ------------------------ some option values --------------------------
@@ -27,7 +28,7 @@ main(int argc, char *argv[])
   int n, m, nel;
   int i;
   int nr_lp, inumpinf, inumdinf, iprobstat, flag, highs_stat;
-  double q, rsumpinf, rsumdinf, sinf, mx_inf, highs_q;
+  double objective_function_value, rsumpinf, rsumdinf, sinf, mx_inf, highs_q;
 
   double *dobj, *dclo, *dcup, *drlo, *drup, *dels, *dels2, *s;
   double *d, *lam, *clp, *lamrow;
@@ -42,6 +43,7 @@ main(int argc, char *argv[])
 
   HighsBasis basis;
   HighsStatus return_status;
+  HighsModelStatus lp_status, lp_status_scaled;
 
   // ------------------------ read data file  --------------------------
   // (in matlab format with hexadecimal floating point number format)
@@ -247,70 +249,78 @@ main(int argc, char *argv[])
   // Set HiGHS options: in src/lp_data/HighsOptions.h
   highs.setHighsOptionValue("primal_feasibility_tolerance", RTOLPINF);
   highs.setHighsOptionValue("simplex_iteration_limit", IMAXITER);
-  highs.setHighsOptionValue("presolve", "off");
+    if (set_stat)
+    highs.setHighsOptionValue("presolve", "off");
 
-  //highs.writeModel("ml.mps");
+    //  highs.writeModel("ml.mps");
   return_status = highs.run();
-   
+  lp_status = highs.getModelStatus(false); //original (unscaled) lp
+  lp_status_scaled = highs.getModelStatus(true);  //scaled lp
+  HighsInfo info = highs.getHighsInfo();
+
+  printf("Unscaled status: %s\n", highs.highsModelStatusToString(lp_status).c_str());
+  printf("Scaled status  : %s\n", highs.highsModelStatusToString(lp_status_scaled).c_str());
+
   //highs.setHighsOptionValue("message_level", 0);
 
-  const HighsSolution& solution = highs.getSolution();
-  for(i=0;i<n;i++){
-    d[i]= solution.col_value[i];
-    lam[i]= solution.col_dual[i];
-  }
-  for(i=0;i<m;i++){
-    clp[i] = solution.row_value[i];
-    lamrow[i] = solution.row_dual[i];
-  }
-  //    highs.getSolution(d, lam, clp, lamrow);
-  
-   
   const HighsInfo& highs_info = highs.getHighsInfo();
-  q = highs_info.objective_function_value;
+  objective_function_value = highs_info.objective_function_value;
   nr_lp = highs_info.simplex_iteration_count;
 
-  for(i=0;i<n;i++){
-    if (d[i]-dcup[i]>1e-5  || dclo[i]-d[i] > 1e-5) {
-      //      cout << i << " " << dclo[i] << " " << d[i] << " " << dcup[i] << "\n";
+  const HighsSolution& solution = highs.getSolution();
+  if (solution.col_value.size()) {
+    for(i=0;i<n;i++){
+      d[i]= solution.col_value[i];
+      lam[i]= solution.col_dual[i];
     }
-    //cout << i << " " << lam(i) << "\n";
-    //normd = max(normd,fabs(d[i]/s[i]));
-  }
+    for(i=0;i<m;i++){
+      clp[i] = solution.row_value[i];
+      lamrow[i] = solution.row_dual[i];
+    }
+  //    highs.getSolution(d, lam, clp, lamrow);
+   
+    for(i=0;i<n;i++){
+      if (d[i]-dcup[i]>1e-5  || dclo[i]-d[i] > 1e-5) {
+	//      cout << i << " " << dclo[i] << " " << d[i] << " " << dcup[i] << "\n";
+      }
+      //cout << i << " " << lam(i) << "\n";
+      //normd = max(normd,fabs(d[i]/s[i]));
+    }
 
 
- for(i=0;i<n;i++){
-    // FIXME: s[i] was dspace(ncolscales-1+i). Is that correct?
-    mx_inf = max(mx_inf, max(
-			     (d[i]-dcup[i])*s[i],
-			     (dclo[i]-d[i])*s[i]));
-    //cout << i << " " << s[i] << "\n";
-  }
-  for (i=0;i<m;i++){
-    // FIXME: s[n+i] was dspace(nrowscales-1+i). Is that correct?
-    mx_inf = max(mx_inf, max(
-			     (clp[i]-drup[i])*s[n+i],
-			     (drlo[i]-clp[i])*s[n+i]));
-    //cout << i << " " << s[n+i] << "\n";
-  }
+    for(i=0;i<n;i++){
+      // FIXME: s[i] was dspace(ncolscales-1+i). Is that correct?
+      mx_inf = max(mx_inf, max(
+			       (d[i]-dcup[i])*s[i],
+			       (dclo[i]-d[i])*s[i]));
+      //cout << i << " " << s[i] << "\n";
+    }
+    for (i=0;i<m;i++){
+      // FIXME: s[n+i] was dspace(nrowscales-1+i). Is that correct?
+      mx_inf = max(mx_inf, max(
+			       (clp[i]-drup[i])*s[n+i],
+			       (drlo[i]-clp[i])*s[n+i]));
+      //cout << i << " " << s[n+i] << "\n";
+    }
   
-  inumpinf = 0;
-  rsumpinf = 0.0;
-  for(i=0;i<n;i++){
-    sinf =  max(max(d[i]-dcup[i], dclo[i]-d[i]),0.);
-    //sinf = scalar infeasibility
-    if (sinf>RTOLPINF) inumpinf++;
-    rsumpinf += sinf;
-  }
-  for(i=0;i<m;i++){
-    sinf =  max(max(clp[i]-drup[i], drlo[i]-clp[i]),0.);
-    if (sinf>RTOLPINF) inumpinf++;
-    rsumpinf += sinf;
-  }
+    inumpinf = 0;
+    rsumpinf = 0.0;
+    for(i=0;i<n;i++){
+      sinf =  max(max(d[i]-dcup[i], dclo[i]-d[i]),0.);
+      //sinf = scalar infeasibility
+      if (sinf>RTOLPINF) inumpinf++;
+      rsumpinf += sinf;
+    }
+    for(i=0;i<m;i++){
+      sinf =  max(max(clp[i]-drup[i], drlo[i]-clp[i]),0.);
+      if (sinf>RTOLPINF) inumpinf++;
+      rsumpinf += sinf;
+    }
   
-  printf("#primal inf = %d\n", inumpinf);
-  printf("#    suminf = %f\n", rsumpinf);
-  printf("#    maxinf = %f\n", mx_inf);
+    printf("#primal inf = %d\n", inumpinf);
+    printf("#    suminf = %f\n", rsumpinf);
+    printf("#    maxinf = %f\n", mx_inf);
+  }
   printf("#   lp_iter = %d\n", nr_lp);
 
 }
